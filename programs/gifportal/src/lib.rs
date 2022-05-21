eef840bae5004675ded1bd6998b47d5d217436da
Daniel Morales <danieldev0612@gmail.com>
2022-05-21 10:24:58 -0700

    Allow users send SOL to GIF posters

--- programs/gifportal/src/lib.rs
+++ programs/gifportal/src/lib.rs
@@ -27,6 +27,21 @@ pub mod gifportal {
         base_account.total_gifs += 1;
         Ok(())
     }
+
+    pub fn send_sol(ctx: Context<SendSol>, amount: u64) -> ProgramResult {
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
+            &ctx.accounts.from.key(),
+            &ctx.accounts.to.key(),
+            amount,
+        );
+        anchor_lang::solana_program::program::invoke(
+            &ix,
+            &[
+                ctx.accounts.from.to_account_info(),
+                ctx.accounts.to.to_account_info(),
+            ],
+        )
+    }
 }
 
 #[derive(Accounts)]
@@ -45,6 +60,15 @@ pub struct AddGif<'info> {
     pub base_account: Account<'info, BaseAccount>,
     #[account(mut)]
     pub user: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct SendSol<'info> {
+    #[account(mut)]
+    from: Signer<'info>,
+    #[account(mut)]
+    to: AccountInfo<'info>,
+    system_program: Program<'info, System>,
 }
 
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
