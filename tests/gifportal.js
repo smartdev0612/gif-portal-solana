1bb281cafe4300aa55cbf0ab7119b2c4bec7897b
Daniel Morales <danieldev0612@gmail.com>
2022-05-20 23:49:53 -0700

    Create add program and test script

--- tests/gifportal.js
+++ tests/gifportal.js
@@ -1,13 +1,49 @@
-const anchor = require("@project-serum/anchor");
+const anchor = require('@project-serum/anchor')
 
-describe("gifportal", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+const main = async () => {
+  console.log("Starting tests...")
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Gifportal;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  const provider = anchor.getProvider()
+  anchor.setProvider(provider)
+  const program = anchor.workspace.Gifportal
+
+  const baseAccount = anchor.web3.Keypair.generate()
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId
+    },
+    signers: [baseAccount]
+  })
+  console.log("Your transaction signature ", tx)
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey)
+  console.log("GIF Count", account.totalGifs.toString())
+
+  await program.rpc.addGif(
+    "https://i.gifer.com/2GU.gif",
+    {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+      }
+    }
+  )
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey)
+  console.log("GIF Count", account.totalGifs.toString())
+  console.log("GIF List", account.gifList)
+}
+
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+runMain()
